89c41512031970b5277c25bd4973bcdc5023571d
liangjialin <liangjialin@tbkt.cn>
2018-03-04 15:25:39 +0800

    修改家里的网络为192.168.0.9：8001

--- src/main/java/com/TrictSystem/Tools/Constant.java
+++ src/main/java/com/TrictSystem/Tools/Constant.java
@@ -5,7 +5,7 @@ package com.TrictSystem.Tools;
  */
 
 public class Constant {
-    public static String domain_home = "http://192.168.0.103:7777";
+    public static String domain_home = "http://192.168.0.9:8001";
     public static String domain_compay = "http://192.168.2.201:8001";
     public static String login_url = "/account/user/login";
     public static String get_all_ticket = "/ticket/get_all_ticket_by_type";
